21735355fd56f086466aae5cf7c7e7634de135e4
soon.good <playmymind@daum.net>
2022-05-18 12:17:05 +0900

    (backend-tdd) 종목리스트 파싱 : StockMeta 모델 매핑 추가

---
+++ backend/calculator/src/test/java/io/stock/kr/calculator/dynamo/StockMeta.java
@@ -0,0 +1,46 @@
+package io.stock.kr.calculator.dynamo;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConvertedEnum;
+import io.stock.kr.calculator.common.types.CrawlingVendorType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.annotation.Id;
+
+@ToString
+@EnableScan
+@DynamoDBTable(tableName = "StockMeta")
+public class StockMeta {
+    public StockMeta(){}
+
+    @Id
+    @Getter @Setter
+    @DynamoDBHashKey(attributeName = "ticker")
+    private String ticker;
+
+    @Getter @Setter
+    @DynamoDBAttribute
+    private String companyName;
+
+    @Getter @Setter
+    @DynamoDBAttribute
+    private String vendorCode;
+
+    @Getter @Setter
+    @DynamoDBTypeConvertedEnum
+    @DynamoDBAttribute(attributeName = "vendorType")
+    private CrawlingVendorType vendorType;
+
+    @Builder
+    public StockMeta(String ticker, String companyName, String vendorCode, CrawlingVendorType vendorType){
+        this.ticker = ticker;
+        this.companyName = companyName;
+        this.vendorCode = vendorCode;
+        this.vendorType = vendorType;
+    }
+}
