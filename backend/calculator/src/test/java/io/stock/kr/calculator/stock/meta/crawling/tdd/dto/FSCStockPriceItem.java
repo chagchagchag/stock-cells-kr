9caf1641c33b7dde2ea1ce970af0abe08b108cd8
soon.good <playmymind@daum.net>
2022-05-20 22:34:42 +0900

    (backend-tdd) 필드 잘못 지정한 부분들 수정

--- backend/calculator/src/test/java/io/stock/kr/calculator/stock/meta/crawling/tdd/dto/FSCStockPriceItem.java
+++ backend/calculator/src/test/java/io/stock/kr/calculator/stock/meta/crawling/tdd/dto/FSCStockPriceItem.java
@@ -1,23 +1,57 @@
 package io.stock.kr.calculator.stock.meta.crawling.tdd.dto;
 
+import io.stock.kr.calculator.stock.price.StockPriceDto;
+import io.stock.kr.calculator.stock.price.redis.StockPriceRedis;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class FSCStockPriceItem {
-    private String baseDt;
-    private String strnCd;
+    private String basDt;
+    private String srtnCd;
     private String isinCd;
     private String itmsNm;
     private String mrktCtg;
     private String vs;
-    private Long mkp;   // open
-    private Long hipr;  // high
-    private Long lopr;  // low
-    private Long clpr;  // close
+    private String mkp;   // open
+    private String hipr;  // high
+    private String lopr;  // low
+    private String clpr;  // close
+
+    public StockPriceRedis toStockPriceRedis(){
+        return StockPriceRedis.builder()
+                .tradeDt(this.getBasDt())
+                .srtnCd(this.getSrtnCd())
+                .isinCd(this.getIsinCd())
+                .itmsNm(this.getItmsNm())
+                .mrktCtg(this.getMrktCtg())
+                .vs(this.getVs())
+                .open(this.getMkp())
+                .high(this.getHipr())
+                .low(this.getLopr())
+                .close(this.getClpr())
+                .build();
+    }
+
+    public StockPriceDto toStockPriceDto(){
+        return StockPriceDto.builder()
+                .basDt(this.getBasDt())
+                .srtnCd(this.getSrtnCd())
+                .isinCd(this.getIsinCd())
+                .itmsNm(this.getItmsNm())
+                .mrktCtg(this.getMrktCtg())
+                .vs(this.getVs())
+                .mkp(new BigDecimal(this.getMkp()))
+                .hipr(new BigDecimal(this.getHipr()))
+                .lopr(new BigDecimal(this.getLopr()))
+                .clpr(new BigDecimal(this.getClpr()))
+                .build();
+    }
 }
