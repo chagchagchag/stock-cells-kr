456d489a26b92ae8384521dd1fe628b3e5bbb9e4
soon.good <playmymind@daum.net>
2022-05-25 01:06:47 +0900

    (backend-tdd) 모델 변경사항 반영 (Partition Key and Sort Key)

--- backend/calculator/src/main/java/io/stock/kr/calculator/stock/price/repository/dynamo/PriceDayRepository.java
+++ backend/calculator/src/main/java/io/stock/kr/calculator/stock/price/repository/dynamo/PriceDayRepository.java
@@ -1,6 +1,11 @@
 package io.stock.kr.calculator.stock.price.repository.dynamo;
 
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PriceDayRepository extends CrudRepository<PriceDayDocument, String> {
+import java.util.List;
+
+@EnableScan
+public interface PriceDayRepository extends CrudRepository<PriceDayDocument, PriceDayDocumentId> {
+    List<PriceDayDocument> findAll();
 }
