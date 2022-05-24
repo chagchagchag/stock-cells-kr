211e4b7037bb90ccc1a1e0bf7560c59de3fb5501
soon.good <playmymind@daum.net>
2022-05-25 01:04:09 +0900

    (backend-tdd) 모델 변경사항 반영 (Partition Key and Sort Key)

--- backend/calculator/src/main/java/io/stock/kr/calculator/stock/price/repository/dynamo/PriceDayDocument.java
+++ backend/calculator/src/main/java/io/stock/kr/calculator/stock/price/repository/dynamo/PriceDayDocument.java
@@ -5,46 +5,66 @@ import io.stock.kr.calculator.common.LocalDateTimeConverter;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
-@Getter @Setter
-@DynamoDBTable(tableName = "PriceDayKr")
+@EnableScan
+@DynamoDBTable(tableName = "PriceDay")
 public class PriceDayDocument {
     @Id
-    @DynamoDBAttribute
-    @DynamoDBHashKey
-    @DynamoDBIndexHashKey(globalSecondaryIndexNames = "tickerTradeDtGSI")
-    String ticker;
+    private PriceDayDocumentId priceDayDocumentId;
 
-    @DynamoDBAttribute
+    @DynamoDBHashKey(attributeName = "ticker")
+//    @DynamoDBIndexHashKey(globalSecondaryIndexName = "PriceDayTickerTradeDtGSI")
+    public String getTicker(){
+        return Optional.ofNullable(priceDayDocumentId.getTicker()).orElse("-");
+    }
+
+    public void setTicker(String ticker){
+        if(Optional.ofNullable(priceDayDocumentId).isEmpty())
+            priceDayDocumentId = new PriceDayDocumentId();
+        priceDayDocumentId.setTicker(ticker);
+    }
+
+    @DynamoDBRangeKey(attributeName = "tradeDt")
+//    @DynamoDBIndexRangeKey(globalSecondaryIndexName = "PriceDayTickerTradeDtGSI")
     @DynamoDBTypeConverted(converter = LocalDateTimeConverter.class)
-    @DynamoDBIndexRangeKey(globalSecondaryIndexName = "tickerTradeDtGSI")
-    LocalDateTime tradeDt;
+    public LocalDateTime getTradeDt(){
+        return priceDayDocumentId.getTradeDt();
+    }
+
+    public void setTradeDt(LocalDateTime tradeDt){
+        priceDayDocumentId.setTradeDt(tradeDt);
+    }
 
     @DynamoDBAttribute
+    @Getter @Setter
     private BigDecimal open;
 
     @DynamoDBAttribute
+    @Getter @Setter
     private BigDecimal high;
 
     @DynamoDBAttribute
+    @Getter @Setter
     private BigDecimal low;
 
     @DynamoDBAttribute
+    @Getter @Setter
     private BigDecimal close;
 
     public PriceDayDocument(){}
 
     @Builder
     public PriceDayDocument(
-            String ticker, LocalDateTime tradeDt,
+            PriceDayDocumentId priceDayDocumentId,
             BigDecimal open, BigDecimal high, BigDecimal low, BigDecimal close
     ){
-        this.ticker = ticker;
-        this.tradeDt = tradeDt;
+        this.priceDayDocumentId = priceDayDocumentId;
         this.open = open;
         this.high = high;
         this.low = low;
