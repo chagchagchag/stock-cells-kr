36dc6a8632ac97a8f0fae777b95e78d877385ac1
soon.good <playmymind@daum.net>
2022-06-13 13:32:57 +0900

    (backend) Web 계층 Exception Handler 추가 = 테스트 코드

--- backend/calculator/src/test/java/io/stock/kr/calculator/stock/price/fakeresponsecontroller/GetStockPriceListTest.java
+++ backend/calculator/src/test/java/io/stock/kr/calculator/stock/price/fakeresponsecontroller/GetStockPriceListTest.java
@@ -1,15 +1,14 @@
 package io.stock.kr.calculator.stock.price.fakeresponsecontroller;
 
+import io.stock.kr.calculator.common.exception.type.DateExceptionType;
 import io.stock.kr.calculator.common.response.CommonResponse;
-import io.stock.kr.calculator.common.response.ResponseType;
-import io.stock.kr.calculator.stock.price.FakeResponseController;
-import io.stock.kr.calculator.stock.price.response.StockPriceItem;
-import org.assertj.core.api.Assertions;
+import io.stock.kr.calculator.stock.price.api.FakeResponseController;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,40 +17,31 @@ public class GetStockPriceListTest {
     @Test
     public void TEST_비어있는_기간데이터가_오면_NOT_OK_를_리턴해야한다(){
         FakeResponseController controller = new FakeResponseController();
-        Mono<CommonResponse<Flux<StockPriceItem>>> stockPriceList = controller.getStockPriceList(null, null);
+        Mono<CommonResponse<String>> stockPriceList = controller.getStockPriceList(null, null);
 
         stockPriceList.subscribe(commonResponse -> {
-            assertThat(commonResponse.getStatus()).isEqualTo(ResponseType.DATE_NULL.getHttpStatusCode());
-            assertThat(commonResponse.getDescription()).isEqualTo(ResponseType.DATE_NULL.getDescription());
+            assertThat(commonResponse.getResultCode()).isEqualTo(DateExceptionType.DATE_NULL.getResultCode());
+            assertThat(commonResponse.getDescription()).isEqualTo(DateExceptionType.DATE_NULL.getDescription());
 
-            Flux<StockPriceItem> body = commonResponse.getBody();
-            StepVerifier.create(body)
-                        .expectNextCount(1);
+//            Flux<StockPriceItem> body = commonResponse.getBody();
+//            StepVerifier.create(body)
+//                        .expectNextCount(1);
         });
     }
 
     @Test
-    public void TEST_모노_테스트(){
-        Mono<String> m = Mono.just("Start");
+    public void TEST_잘못된_기간데이터가_오면_NOT_OK_를_리턴해야한다(){
+        FakeResponseController controller = new FakeResponseController();
+        Mono<CommonResponse<String>> stockPriceList = controller.getStockPriceList("2022-11-22", "2022-11-23");
 
-        StepVerifier.create(m)
-                .expectNext("Start")
-                .verifyComplete();
+        stockPriceList.subscribe(commonResponse -> {
+            assertThat(commonResponse.getResultCode()).isEqualTo(DateExceptionType.DATE_FORMAT_INVALID.getResultCode());
+            assertThat(commonResponse.getDescription()).isEqualTo(DateExceptionType.DATE_FORMAT_INVALID.getDescription());
+
+//            Flux<StockPriceItem> body = commonResponse.getBody();
+//            StepVerifier.create(body).expectNextCount(1);
+        });
     }
 
-    @Test
-    public void TEST_플럭스_테스트(){
-        Flux<String> flux = Flux.just("Start", "From", "Basic");
 
-        StepVerifier.create(flux)
-                .expectNext("Start")
-                .expectNext("From")
-                .expectNext("Basic")
-                .verifyComplete();
-
-        StepVerifier.create(flux)
-                .expectNextCount(3)
-                .verifyComplete();
-
-    }
 }
