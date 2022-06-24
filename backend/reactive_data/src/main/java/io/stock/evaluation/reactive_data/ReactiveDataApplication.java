642009ee8a2f3d79444afd0a1e669f10e82d8567
soon.good <playmymind@daum.net>
2022-06-24 10:44:32 +0900

    (backend) r2dbc 설정을 잠시 꺼둠 = DynamoDB or MongoDB 관련 확정되는 사항들이 생기면 다시 추가 예정

--- backend/reactive_data/src/main/java/io/stock/evaluation/reactive_data/ReactiveDataApplication.java
+++ backend/reactive_data/src/main/java/io/stock/evaluation/reactive_data/ReactiveDataApplication.java
@@ -2,8 +2,11 @@ package io.stock.evaluation.reactive_data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+// 아래 설정은 임시. DynamoDB 관련 설정이 마무리 되면 아래의 설정은 제거 예정
+@SpringBootApplication(exclude = R2dbcAutoConfiguration.class)
 public class ReactiveDataApplication {
 
 	public static void main(String[] args) {
