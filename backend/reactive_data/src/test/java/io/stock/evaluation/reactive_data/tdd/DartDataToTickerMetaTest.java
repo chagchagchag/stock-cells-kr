a91e9a84e277de56b904fe77e3ff11e2a56e0f5d
soon.good <playmymind@daum.net>
2022-06-25 15:12:10 +0900

    (backend) 코드 정리 = 커밋 안하고 쌓아두다가 정리중.

--- backend/reactive_data/src/test/java/io/stock/evaluation/reactive_data/tdd/DartDataToTickerMetaTest.java
+++ backend/reactive_data/src/test/java/io/stock/evaluation/reactive_data/tdd/DartDataToTickerMetaTest.java
@@ -3,12 +3,18 @@ package io.stock.evaluation.reactive_data.tdd;
 
 import io.stock.evaluation.reactive_data.ticker.meta.dto.TickerMetaItem;
 import io.stock.evaluation.reactive_data.ticker.meta.external.DartDataConverter;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ReactiveHashOperations;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.test.context.ActiveProfiles;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,10 +45,55 @@ public class DartDataToTickerMetaTest {
                 tickerMetaItem ->
                         tickerMetaMapOps
                                 .opsForValue()
-                                .set("TICKER-"+tickerMetaItem.getTicker(), tickerMetaItem).block()
+                                .set("SEARCH-TICKER-"+tickerMetaItem.getTicker(), tickerMetaItem).block()
         );
 
-        Long count = tickerMetaMapOps.keys("TICKER-*").count().block();
+        Long count = tickerMetaMapOps.keys("SEARCH-TICKER-*").count().block();
+        assertThat(count).isNotZero();
+    }
+
+
+    // 검색어 추천을 위해 아래와 같은 종류의 키/밸류를 넣어준다.
+    // - ticker(종목코드)  : TickerMetaItem
+    // - 종목명            : TickerMetaItem
+    @Test
+    public void TEST_REDIS_PUT_TICKER_LIST2(){
+        DartDataConverter converter = new DartDataConverter();
+        Flux<TickerMetaItem> tickerMetaItemFlux = converter.processTickers();
+
+        // 데이터가 모두 저장되는지 여부를 테스트해야 하기에 .block() 을 사용
+        tickerMetaItemFlux.subscribe(
+                tickerMetaItem ->{
+                    tickerMetaMapOps
+                            .opsForZSet()
+                            .add("SEARCH-STOCK-"+tickerMetaItem.getTicker(), tickerMetaItem, 0).block();
+
+                    tickerMetaMapOps
+                            .opsForValue()
+                            .set("SEARCH-STOCK-"+tickerMetaItem.getCompanyName(), tickerMetaItem, 0).block();
+                }
+        );
+
+        Long count = tickerMetaMapOps.keys("SEARCH-STOCK-*").count().block();
+        assertThat(count).isNotZero();
+    }
+
+    @Disabled
+    @Test
+    public void TEST_REDIS_AUTO_COMPLETE_SIMPLE(){
+        DartDataConverter converter = new DartDataConverter();
+        Flux<TickerMetaItem> tickerMetaItemFlux = converter.processTickers();
+
+        tickerMetaItemFlux.subscribe(
+                tickerMetaItem -> {
+                    tickerMetaMapOps
+                            .opsForHash()
+                            .put("AUTO-COMPLETE", tickerMetaItem.getTicker(), tickerMetaItem).block();
+                }
+        );
+
+//        Long count = tickerMetaMapOps.keys("AUTO-COMPLETE-*").count().block();
+        long count = tickerMetaMapOps.opsForHash().entries("AUTO-COMPLETE").count().block();
         assertThat(count).isNotZero();
     }
 }
