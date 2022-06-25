94a7c450d7b5bbdaf79e4c8d5232370793d518a0
soon.good <playmymind@daum.net>
2022-06-25 22:10:19 +0900

    (backend) 기본생성자 추가...

--- backend/reactive_data/src/main/java/io/stock/evaluation/reactive_data/ticker/meta/dto/TickerMetaItem.java
+++ backend/reactive_data/src/main/java/io/stock/evaluation/reactive_data/ticker/meta/dto/TickerMetaItem.java
@@ -1,17 +1,16 @@
 package io.stock.evaluation.reactive_data.ticker.meta.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Getter
 @ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TickerMetaItem implements Serializable {
-    private final String ticker;
-    private final String companyName;
-    private final String tickerCode; // vendor specific code
+    private String ticker;
+    private String companyName;
+    private String tickerCode; // vendor specific code
 
     @Builder
     public TickerMetaItem(String ticker, String companyName, String tickerCode){
