70240b4bc2f3c73def17864b402595ac8fa7492e
soon.good <playmymind@daum.net>
2022-07-01 15:34:25 +0900

    (fronend) 검색어 추천 ui 구성
    = 테스트를 위해 일단은 로딩시에 바로 뜨게끔 해둠

--- frontend/stock-calculator-kr/src/search/SearchInput.jsx
+++ frontend/stock-calculator-kr/src/search/SearchInput.jsx
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
 
 class SearchInput extends Component {
+	state = {
+		list: []
+	};
+	
+	componentDidMount(){
+		fetch('data/dump-stock-list.json')
+		.then(function(result){
+			return result.json(); 
+		})
+		.then(function(json){
+			console.log(json);
+			this.setState({list:json});
+		}.bind(this));
+
+	}
+
 	render() {
+		var searchResultEl = [];
+		for(var i=0; i<this.state.list.length; i++){
+			var each = this.state.list[i];
+			searchResultEl.push(
+				<div key={each.corpCode} className='card card-body mb-1'>
+					<small>회사명 : {each.name} / 종목코드 : {each.corpCode}</small>
+				</div>
+			)
+		}
+
 		return (
-		<div class="row">
-			<div class="col-md-6 m-auto">
-			<h3 class="text-center mb-3">
-				<i class="fa-solid fa-car-tunnel"></i> STOCK CELLS
+		<div className="row">
+			<div className="col-md-6 m-auto">
+			<h3 className="text-center mb-3">
+				<i className="fa-solid fa-car-tunnel"></i> STOCK CELLS
 			</h3>
 			<input 
 				type="text" 
 				id="search" 
-				class="form-control form-control-lg" 
+				className="form-control form-control-lg" 
 				placeholder="종목 검색"/>
 			</div>
-        	<div id="match-list"></div>
+        	{/* <div id="match-list"></div> */}
+			<div>
+				{searchResultEl}
+			</div>
 		</div>
 		);
 	}
