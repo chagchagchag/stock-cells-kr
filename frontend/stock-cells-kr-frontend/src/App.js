3cf8f610804e626624dbfac6c7e4df4508282343
soon.good <playmymind@daum.net>
2022-07-06 18:43:01 +0900

    (frontend) material-table 을 활용한 테이블 기본 기능 구현

--- frontend/stock-cells-kr-frontend/src/App.js
+++ frontend/stock-cells-kr-frontend/src/App.js
@@ -3,14 +3,18 @@ import './theme/flatly-bootstrap.min.css';
 
 import './search/SearchInput';
 import SearchInput from './search/SearchInput';
-import PriceCells from './cells/react-bootstrap-table2/PriceCells';
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
+//-- react-bootstrap-table2
+// import PriceCells from './demo/cells/react-bootstrap-table2/PriceCells';
+// import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
+import PriceCells from './demo/cells/material-table/PriceCells';
 
 function App() {
   return (
     <div className='container mt-5'>
       <SearchInput></SearchInput>
-      {/* <PriceCells></PriceCells> */}
+      <PriceCells></PriceCells>
     </div>
   );
 }
