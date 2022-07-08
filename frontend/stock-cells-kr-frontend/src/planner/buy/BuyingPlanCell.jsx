44e2759a787d6e51b867ae0ab876d0dbeb7a6825
soon.good <playmymind@daum.net>
2022-07-08 13:11:08 +0900

    (frontend) 가격 그리드 구현 (데이터 연동x)
    = 빡셨다... 와... 진짜 ..

--- frontend/stock-cells-kr-frontend/src/planner/buy/BuyingPlanCell.jsx
+++ frontend/stock-cells-kr-frontend/src/planner/buy/BuyingPlanCell.jsx
@@ -15,14 +15,43 @@ class BuyPlannerCell extends Component {
 				editable: true,
 				resizable: true,
 				minWidth: 100,
+				// https://www.ag-grid.com/react-data-grid/column-headers/
+				wrapHeaderText: true,  // 응? 이건 무슨 옵션이지?? 다시 찾아봐야 함 
+				autoHeaderHight: true, // 너비를 넘어가면 Hight를 늘리게끔 조절
 				flex: 1,
 			},
 			popupParent: document.body,
-			columnDefs: [
-				{ field: 'price' }, {field: 'changedRatio'}, { field: 'changedPrice' }, {field: 'chnagedPer'}
+			// columnDefs: [
+			// 	{ field: 'price' }, {field: 'changedRatio'}, { field: 'changedPrice' }, {field: 'chnagedPer'}
+			// ],
+			columnDefs:[
+				{
+					headerName: '가격리스트',
+					children:[
+						{headerName: '회사명', field: 'company', minWidth: 50, resizable: true},
+						{headerName: '조회 가격 (2022/07/08)', minWidth: 120, field: 'price', resizable: true},
+						{headerName: '등락율(+/-)', field: 'priceRatio', resizable: true},
+						{headerName: '적용 가격', field: 'changedPrice', resizable: true},
+						{headerName: '적용 PER', field: 'changedPer', resizable: true},
+					]
+				},
+				{
+					headerName: '실적, 분기 데이터',
+					children:[
+						{headerName: 'PER', field: 'per', resizable: true},
+						{headerName: 'PES', field: 'eps', resizable: true},
+						{headerName: '당좌비율', field: 'cash'},
+						{headerName: '유보율', field: 'cashcash'} // 유보율 ...
+					]
+				}
 			],
 			rowData: [
-				{price: 35000, changedRatio: 90, changedPrice: 35000*0.9, changedPer: 111}
+				{company: '삼성전자', price: 53400, changedPrice: 53300, priceRatio: 99, changedPer: 5, per: 6, eps: 55555, cash: 11111, cashcash: 11111},
+				{company: '삼성전자', price: 53400, changedPrice: 53300, priceRatio: 98, changedPer: 5, per: 6, eps: 55555, cash: 11111, cashcash: 11111},
+				{company: '삼성전자', price: 53400, changedPrice: 53300, priceRatio: 97, changedPer: 5, per: 6, eps: 55555, cash: 11111, cashcash: 11111},
+				{company: '삼성전자', price: 53400, changedPrice: 53300, priceRatio: 96, changedPer: 5, per: 6, eps: 55555, cash: 11111, cashcash: 11111},
+				{company: '삼성전자', price: 53400, changedPrice: 53300, priceRatio: 95, changedPer: 5, per: 6, eps: 55555, cash: 11111, cashcash: 11111},
+				// {price: 35000, changedRatio: 90, changedPrice: 35000*0.9, changedPer: 111}
 			],
 		};
 	}
@@ -45,9 +74,9 @@ class BuyPlannerCell extends Component {
 			<div style={{width: '100%', height: '100%'}}>
 				<div style={{ display: 'flex', flexDirection: 'column', height: '100%'}}>
 					<div style={{ margin: '10px 0' }}>
-						<button onClick={() => this.onBtnUpdate()}>
+						{/* <button onClick={() => this.onBtnUpdate()}>
 							Show CSV export content text
-						</button>
+						</button> */}
 						<button onClick={() => this.onBtnExport()}>
 							Download CSV export file
 						</button>
@@ -56,7 +85,6 @@ class BuyPlannerCell extends Component {
 					<div style={{ flex: '1 1 0', position: 'relative' }}>
 						<div id="gridContainer" style={{height: '500px', width: '100%'}}>
 							<div style={{height: '100%', width: '100%'}} className="ag-theme-alpine-dark">
-							{/* <div style={{height: '100%'}} className="ag-theme-alpine"> */}
 								<AgGridReact 
 									defaultColDef={this.state.defaultColDef}
 									suppressExcelExport={true}
@@ -65,10 +93,6 @@ class BuyPlannerCell extends Component {
 									rowData={this.state.rowData}
 									onGridReady={this.onGridReady}
 								/>
-								{/* <AgGridReact
-									rowData={this.state.rowData}
-									columnDefs={this.state.columnDefs}
-								/> */}
 							</div>
 						</div>
 						{/* <textarea id="csvResult" defaultValue={"Click the Show CSV export content button to view exported CSV here"}>
