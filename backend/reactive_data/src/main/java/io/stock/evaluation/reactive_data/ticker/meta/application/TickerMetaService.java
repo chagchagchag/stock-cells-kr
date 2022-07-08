fdbdeccaab8c18e44e37cfab20b0610baf266b8e
soon.good <playmymind@daum.net>
2022-07-08 14:49:42 +0900

    (backend) 커밋안되있던 이전 버전 커밋 =

--- backend/reactive_data/src/main/java/io/stock/evaluation/reactive_data/ticker/meta/application/TickerMetaService.java
+++ backend/reactive_data/src/main/java/io/stock/evaluation/reactive_data/ticker/meta/application/TickerMetaService.java
@@ -1,6 +1,8 @@
 package io.stock.evaluation.reactive_data.ticker.meta.application;
 
 import io.stock.evaluation.reactive_data.ticker.meta.dto.TickerMetaItem;
+import io.stock.evaluation.reactive_data.ticker.meta.external.DartDataConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -8,20 +10,24 @@ import reactor.core.publisher.Flux;
 @Service
 public class TickerMetaService {
 
-    private final ReactiveRedisOperations<String, String> tickerMapReactiveRedisOperation;
+    private final ReactiveRedisOperations<String, TickerMetaItem> tickerMetaMap;
+    private final DartDataConverter dartDataConverter;
 
     public TickerMetaService(
-        ReactiveRedisOperations<String, String> tickerMapReactiveRedisOperation
+        @Qualifier("tickerMetaMapReactiveRedisOperation") ReactiveRedisOperations<String, TickerMetaItem> tickerMetaMap,
+        DartDataConverter dartDataconverter
     ){
-        this.tickerMapReactiveRedisOperation = tickerMapReactiveRedisOperation;
+        this.tickerMetaMap = tickerMetaMap;
+        this.dartDataConverter = dartDataconverter;
     }
 
     // 종목코드(=Ticker), 종목명, DART CODE 셋중 하나로만 입력해도 종목코드(=Ticker)를 찾아서 리턴한다.
     public Flux<TickerMetaItem> findTickerByAny(String key){
         // TODO::구현 예정
-        return tickerMapReactiveRedisOperation
-                .opsForValue().get(key)
-                .flatMapMany(this::findTickerMetaItemByTicker);
+//        return tickerMetaMap
+//                .opsForValue().get(key)
+//                .flatMapMany(this::findTickerMetaItemByTicker);
+        return Flux.empty();
     }
 
     // ticker 기반으로 TickerMetaItem 을 조회한다.
@@ -29,5 +35,11 @@ public class TickerMetaService {
         // TODO::구현 예정
         // Router, Handler 구현이 완료 된후, 세부 모델을 확정지을 예정.
         return Flux.empty();
+    }
+
+    public void loadTickers(){
+        Flux<TickerMetaItem> tickerMetaItemFlux = dartDataConverter.processTickers();
+        tickerMetaItemFlux
+                .subscribe(tickerMetaItem -> tickerMetaMap.opsForValue().set("TICKER-"+tickerMetaItem.getTicker(), tickerMetaItem));
     }
 }
