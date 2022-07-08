adf296c212d85a01d282484eeae03fe9b2f35712
soon.good <playmymind@daum.net>
2022-07-08 23:07:04 +0900

    (backend) Chain Of Responsibility 잠깐 스킵

---
+++ backend/reactive_data/src/test/java/io/stock/evaluation/reactive_data/langtest/pojo/EvaluationData.java
@@ -0,0 +1,15 @@
+package io.stock.evaluation.reactive_data.langtest.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class EvaluationData {
+    @Setter EvaluationDataStatus status;
+    private final List<EvaluationData> dataList = new ArrayList<>();
+    BigDecimal totalAmount;
+}
---
+++ backend/reactive_data/src/test/java/io/stock/evaluation/reactive_data/langtest/pojo/EvaluationDataStatus.java
@@ -0,0 +1,5 @@
+package io.stock.evaluation.reactive_data.langtest.pojo;
+
+public enum EvaluationDataStatus {
+    CREATED, IN_PROGRESS
+}
