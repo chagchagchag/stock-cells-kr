641d4f993de1f51390fc3955cbea6ab2f88ce5e0
soon.good <playmymind@daum.net>
2022-07-09 16:43:57 +0900

    (backend-tdd) 네이버 파이낸스 크롤링 로직 = 숫자문자열 (컴마, 한국어단위) 파싱/변환 로직 추가

---
+++ backend/reactive_data/src/main/java/io/stock/evaluation/reactive_data/global/common/StringParser.java
@@ -0,0 +1,41 @@
+package io.stock.evaluation.reactive_data.global.common;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.function.Function;
+
+
+public class StringParser {
+    public static Function<String, Optional<BigDecimal>> toDecimal(){
+        return str -> {
+            try {
+                Number parse = NumberFormat.getNumberInstance(Locale.US).parse(str);
+                return Optional.of(new BigDecimal(parse.toString()));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return Optional.empty();
+        };
+    }
+
+    public static Function<String, Optional<BigDecimal>> krStringToDecimal(){
+        return given -> {
+            if(given.contains("조")){
+                int t = given.indexOf("조");
+                StringBuilder test = new StringBuilder(given.substring(0, t)).append(given.substring(t + 2));
+
+                return StringParser.toDecimal().apply(test.toString())
+                                .map(decimal -> {
+                                    BigDecimal multiply = decimal.multiply(BigDecimal.valueOf(100000000));
+                                    return multiply;
+                                });
+            }
+            else{
+                return StringParser.krStringToDecimal().apply(given);
+            }
+        };
+    }
+}
