9e6c8dd987b91925add5a206c6c313981c55a6d1
soon.good <playmymind@daum.net>
2022-07-14 03:06:46 +0900

    (frontend-study) 함수형 컴포넌트, 클래스형 컴포넌트 state 사용방식 예제

--- study/FRONTEND/demo_functional_component/src/App.js
+++ study/FRONTEND/demo_functional_component/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
   return (
@@ -13,19 +13,35 @@ function App() {
 }
 
 class ClassStyleComponent extends React.Component{
+  state = {
+    fruitType : this.props.fruitType
+  };
+
   render(){
     return (
       <div className="container">
-        <h2>클래스 형 컴포넌트 : {this.props.fruitType}</h2>
+        <h2>클래스 형 컴포넌트 : {this.state.fruitType}</h2>
+        <input type="text" onChange={function(e){
+          this.setState({
+            fruitType: e.target.value
+          });
+        }.bind(this)}/>
       </div>
     );
   }
 }
 
 function FunctionalComponent(props){
+  var fruitTypeProps = useState(props.fruitType);
+  var fruitType = fruitTypeProps[0];    // 0 번째 요소는 state 변수
+  var setFruitType = fruitTypeProps[1]; // 1 번째 요소는 setter 함수 
+
   return (
     <div className="container">
-      <h2>함수형 컴포넌트 : {props.fruitType}</h2>
+      <h2>함수형 컴포넌트 : {fruitType}</h2>
+      <input type="text" onChange={function(e){
+        setFruitType(e.target.value);
+      }.bind(this)}/>
     </div>
   );
 }
