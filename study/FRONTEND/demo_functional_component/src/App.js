bfe09b633f74e509ae958d45b2fc445503638397
soon.good <playmymind@daum.net>
2022-07-14 01:55:20 +0900

    (frontend-study) 함수형 컴포넌트, 클래스형 컴포넌트 state 사용로직

--- study/FRONTEND/demo_functional_component/src/App.js
+++ study/FRONTEND/demo_functional_component/src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>컴포넌트 state 를 클래스/함수형 스타일로 구현해보기</h1>
+      <ClassStyleComponent fruitType={"APPLE"}></ClassStyleComponent>
+      <FunctionalComponent fruitType={"BANANA"}></FunctionalComponent>
     </div>
   );
 }
+
+class ClassStyleComponent extends React.Component{
+  render(){
+    return (
+      <div className="container">
+        <h2>클래스 형 컴포넌트 : {this.props.fruitType}</h2>
+      </div>
+    );
+  }
+}
+
+function FunctionalComponent(props){
+  return (
+    <div className="container">
+      <h2>함수형 컴포넌트 : {props.fruitType}</h2>
+    </div>
+  );
+}
+
+
 
 export default App;
