37aaefa1ffd01d80f34dc5a1cae93ca6ed3fff14
soon.good <playmymind@daum.net>
2022-07-21 03:07:28 +0900

    (frontend) react-bootstrap 적용 기본 세팅

--- frontend/stock-cells-kr-frontend/src/App.js
+++ frontend/stock-cells-kr-frontend/src/App.js
@@ -1,8 +1,12 @@
-// -- (start) bootstrap 
+// -- (start) bootstrap 테마 
 // import './theme/flatly-bootstrap.min.css';
 // import './theme/darkly-bootstrap.min.css';
 import './theme/litera-bootstrap.min.css';
 // import './theme/styles.css';
+
+// -- (start) react-bootstrap
+// 참고) https://react-bootstrap.github.io/getting-started/introduction/#css 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import BuyPriceTable from './planner/buy/BuyPriceTable';
 import SearchCompanyInput from './search/components/SearchCompanyInput';
