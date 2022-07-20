50ed0a22a1d928a453e7fb8908117b55ae93296c
soon.good <playmymind@daum.net>
2022-07-21 02:58:37 +0900

    (frontend) 이전버전 로직 제거 및 정리

--- frontend/stock-cells-kr-frontend/src/App.js
+++ frontend/stock-cells-kr-frontend/src/App.js
@@ -1,32 +1,16 @@
 // -- (start) bootstrap 
 // import './theme/flatly-bootstrap.min.css';
 // import './theme/darkly-bootstrap.min.css';
+import './theme/litera-bootstrap.min.css';
+// import './theme/styles.css';
+
 import BuyPriceTable from './planner/buy/BuyPriceTable';
 import SearchCompanyInput from './search/components/SearchCompanyInput';
 import TickerList from './search/components/TickerList';
-import './theme/litera-bootstrap.min.css';
-// import './theme/styles.css';
-//-- react-bootstrap-table2
-// import PriceCells from './demo/cells/react-bootstrap-table2/PriceCells';
-// import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-// -- (end) bootstrap
-
-// -- (start) component, containers
-// import SearchInputComponent from './search/components/SearchInputComponent';
-
-// -- (구버전 ^^;;;)
-// import SearchInputContainer from './search/containers/SearchInputContainer';
-// import BuyingPlanCell from './planner/buy/BuyingPlanCell';
-// import TickerSearchItemContainer from './search/containers/TickerSearchItemContainer';
 
 function App() {
   return (
     <div className='container mt-5'>
-      {/* 구버전 ^^;; */}
-      {/* <SearchInputContainer></SearchInputContainer>
-      <TickerSearchItemContainer></TickerSearchItemContainer>
-      <BuyingPlanCell style={{width: '100%', height: '100%'}}></BuyingPlanCell> */}
-
       <SearchCompanyInput></SearchCompanyInput>
       <TickerList></TickerList>
       <BuyPriceTable></BuyPriceTable>
